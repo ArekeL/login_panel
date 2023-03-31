13591abb0db68a13d04335142940f341e47b98c3
Arek Lechnik <alechnik@gmail.com>
2023-03-31 06:35:34 +0200

    Adding Button to card + small fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 
-import Login from "./components/Login/Login";
+import Button from "./components/UI/Button/Button";
 import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
